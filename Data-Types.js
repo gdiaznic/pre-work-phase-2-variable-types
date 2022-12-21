11a79e5e35d910b191d7cd7323d228421e6f4020
Nicole Diaz <ndiaz18@cnm.edu>
2022-12-21 10:26:07 -0700

    Wrote Variables, data types, typing system and operators.

---
+++ Data-Types.js
@@ -0,0 +1,9 @@
+let obj = {
+    key1: "value 1",
+    key2: 3,
+    obj2: {
+        obj2Key1 : "Value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2Key1);
---
+++ Variables.js
@@ -0,0 +1,9 @@
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
